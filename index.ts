45db658ecc70e5474bc2ca429cad1b46ede62f37
Sébastien Piquemal <sebpiq@gmail.com>
2022-12-08 12:49:28 +0100

    added missing lib exports

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
-export { PdJson } from './src/types'
+export { PdJson, NodeBuilder, NodeBuilders } from './src/types'
 export { default as toDspGraph } from './src/to-dsp-graph'
 export * as validation from './src/validation'
+export * as pdJsonHelpers from './src/pdjson-helpers'
